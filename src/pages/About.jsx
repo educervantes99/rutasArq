cd260f0daae2fb62b0f977ab44fc35bc050acfdf
Eduardo Cervantes <ecervantes@ecomsur.com>
2024-06-22 15:49:54 -0600

    -

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,15 @@
 export const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1 className="siteName">About</h1>
+
+      <div className="siteContent">
+        <p className="siteContentInfo">México es un país con una rica herencia cultural y arqueológica, albergando más de 200 zonas arqueológicas abiertas al público, que representan el legado de antiguas civilizaciones como los mayas, aztecas, olmecas, zapotecas y mixtecas. La diversidad y dispersión geográfica de estos sitios presentan un desafío logístico significativo tanto para los turistas como para los investigadores que desean explorar múltiples ubicaciones en un tiempo limitado.
+          El objetivo de este proyecto es desarrollar una solución eficiente para determinar la ruta más corta que permita visitar todos los sitios arqueológicos en México. Este proyecto busca proporcionar una herramienta que optimice el tiempo y los recursos necesarios para recorrer estas maravillas culturales.
+        </p>
+
+      </div>
+
     </div>
   );
 };
--- src/utils/grafo.js
+++ src/utils/grafo.js
@@ -20,7 +20,10 @@ export const positions = [
     {id: "5-5", info: { x: 325, y: 430, size: 3, label: "Huapoca", color: "#fabfb7" }},
 
     {id: "6", info: { x: 500, y: 340, size: 15, label: "Coahuila", color: "#fdf9c4" }},
+    {id: "6-1", info: { x: 500, y: 370, size: 3, label: "Rincón Colorado", color: "#fdf9c4" }},
+
     {id: "7", info: { x: 600, y: 310, size: 13, label: "Nuevo Leon", color: "#ffda9e" }},
+    {id: "7-1", info: { x: 570, y: 300, size: 3, label: "Boca de Potrerillos", color: "#ffda9e" }},
 
     {id: "8", info: { x: 640, y: 250, size: 15, label: "Tamaulipas", color: "#c5c6c8" }},
     {id: "8-1", info: { x: 660, y: 270, size: 3, label: "El Sabinito", color: "#c5c6c8" }},
@@ -28,10 +31,19 @@ export const positions = [
     {id: "8-3", info: { x: 620, y: 225, size: 3, label: "Balcón de Montezuma", color: "#c5c6c8" }},
 
     {id: "9", info: { x: 300, y: 290, size: 12, label: "Sinaloa", color: "#b2e2f2" }},
+    {id: "9-1", info: { x: 300, y: 260, size: 3, label: "Las Labradas", color: "#b2e2f2" }},
+
     {id: "10", info: { x: 410, y: 290, size: 15, label: "Durango", color: "#ffe4e1" }},
+    {id: "10-1", info: { x: 390, y: 260, size: 3, label: "La Ferrería", color: "#ffe4e1" }},
+
     {id: "11", info: { x: 480, y: 250, size: 15, label: "Zacatecas", color: "#d8f8e1" }},
+
     {id: "12", info: { x: 550, y: 230, size: 15, label: "San Luis", color: "#fcb7af" }},
+    {id: "12-1", info: { x: 560, y: 205, size: 3, label: "Tamohi", color: "#fcb7af" }},
+    {id: "12-2", info: { x: 550, y: 260, size: 3, label: "Tamtoc", color: "#fcb7af" }},
+
     {id: "13", info: { x: 400, y: 200, size: 12, label: "Nayarit", color: "#b0f2c2" }},
+    {id: "13-1", info: { x: 380, y: 180, size: 3, label: "Ixtlán", color: "#b0f2c2" }},
 
     {id: "14", info: { x: 450, y: 160, size: 12, label: "Jalisco", color: "#b0c2f2" }},
     {id: "14-1", info: { x: 450, y: 140, size: 3, label: "Ixtépete", color: "#b0c2f2" }},
@@ -54,13 +66,76 @@ export const positions = [
     {id: "17-4", info: { x: 627, y: 145, size: 3, label: "Pahñú", color: "#ffda9e" }},
 
     {id: "18", info: { x: 720, y: 130, size: 12, label: "Veracruz", color: "#c5c6c8" }},
+    {id: "18-1", info: { x: 720, y: 160, size: 3, label: "Castillo de Teayo", color: "#c5c6c8" }},
+    {id: "18-2", info: { x: 740, y: 150, size: 3, label: "Cempoala", color: "#c5c6c8" }},
+    {id: "18-3", info: { x: 760, y: 140, size: 3, label: "Las Higueras", color: "#c5c6c8" }},
+    {id: "18-4", info: { x: 700, y: 140, size: 3, label: "San Lorenzo", color: "#c5c6c8" }},
+    {id: "18-5", info: { x: 740, y: 110, size: 3, label: "Tajín", color: "#c5c6c8" }},
+    {id: "18-6", info: { x: 710, y: 170, size: 3, label: "El Zapotal", color: "#c5c6c8" }},
+    {id: "18-7", info: { x: 720, y: 170, size: 3, label: "Vega de la Peña", color: "#c5c6c8" }},
+    {id: "18-8", info: { x: 740, y: 165, size: 3, label: "Quiahuiztlán", color: "#c5c6c8" }},
+    {id: "18-9", info: { x: 750, y: 130, size: 3, label: "Cuajilote", color: "#c5c6c8" }},
+    {id: "18-10", info: { x: 745, y: 140, size: 3, label: "Cuyuxquihui", color: "#c5c6c8" }},
+
     {id: "19", info: { x: 520, y: 130, size: 12, label: "Michoacan", color: "#b2e2f2" }},
+    {id: "19-1", info: { x: 520, y: 110, size: 3, label: "Huandacareo", color: "#b2e2f2" }},
+    {id: "19-2", info: { x: 500, y: 115, size: 3, label: "Ihuatzio", color: "#b2e2f2" }},
+    {id: "19-3", info: { x: 530, y: 115, size: 3, label: "San Felipe los Alzati", color: "#b2e2f2" }},
+    {id: "19-4", info: { x: 500, y: 133, size: 3, label: "Tingambato", color: "#b2e2f2" }},
+    {id: "19-5", info: { x: 510, y: 110, size: 3, label: "Tres Cerritos", color: "#b2e2f2" }},
+    {id: "19-6", info: { x: 510, y: 150, size: 3, label: "Tzintzuntzan", color: "#b2e2f2" }},
+
     {id: "20", info: { x: 600, y: 130, size: 9, label: "Mexico", color: "#ffe4e1" }},
+
     {id: "21", info: { x: 600, y: 90, size: 12, label: "Guerrero", color: "#d8f8e1" }},
+    {id: "21-1", info: { x: 600, y: 60, size: 3, label: "Huamuxtitlán", color: "#d8f8e1" }},
+    {id: "21-2", info: { x: 570, y: 90, size: 3, label: "Ixcateopan", color: "#d8f8e1" }},
+    {id: "21-3", info: { x: 580, y: 70, size: 3, label: "La Organera", color: "#d8f8e1" }},
+    {id: "21-4", info: { x: 620, y: 70, size: 3, label: "La Sabana", color: "#d8f8e1" }},
+    {id: "21-5", info: { x: 610, y: 60, size: 3, label: "Cuetlajuchitlán", color: "#d8f8e1" }},
+    {id: "21-6", info: { x: 590, y: 70, size: 3, label: "Palma Sola", color: "#d8f8e1" }},
+    {id: "21-7", info: { x: 560, y: 70, size: 3, label: "Los Tepoltzis", color: "#d8f8e1" }},
+    {id: "21-8", info: { x: 580, y: 50, size: 3, label: "Teopantecuanitlán", color: "#d8f8e1" }},
+    {id: "21-9", info: { x: 620, y: 50, size: 3, label: "Tehuacalco", color: "#d8f8e1" }},
+    {id: "21-10", info: { x: 630, y: 70, size: 3, label: "Soledad de Maciel", color: "#d8f8e1" }},
+
     {id: "22", info: { x: 730, y: 70, size: 12, label: "Oaxaca", color: "#fcb7af" }},
+
     {id: "23", info: { x: 880, y: 50, size: 12, label: "Chiapas", color: "#b0f2c2" }},
+    {id: "23-1", info: { x: 880, y: 20, size: 3, label: "Bonampak", color: "#b0f2c2" }},
+    {id: "23-2", info: { x: 850, y: 40, size: 3, label: "Chincultic", color: "#b0f2c2" }},
+    {id: "23-3", info: { x: 860, y: 30, size: 3, label: "Izapa", color: "#b0f2c2" }},
+    {id: "23-4", info: { x: 870, y: 70, size: 3, label: "Palenque", color: "#b0f2c2" }},
+    {id: "23-5", info: { x: 900, y: 30, size: 3, label: "Tenam", color: "#b0f2c2" }},
+    {id: "23-6", info: { x: 920, y: 40, size: 3, label: "Toniná", color: "#b0f2c2" }},
+    {id: "23-7", info: { x: 860, y: 10, size: 3, label: "Yaxchilán", color: "#b0f2c2" }},
+    {id: "23-8", info: { x: 900, y: 10, size: 3, label: "Chiapa de Corzo", color: "#b0f2c2" }},
+    {id: "23-9", info: { x: 840, y: 30, size: 3, label: "Lagartero", color: "#b0f2c2" }},
+    {id: "23-10", info: { x: 820, y: 30, size: 3, label: "Iglesia Vieja", color: "#b0f2c2" }},
+
     {id: "24", info: { x: 880, y: 100, size: 10, label: "Tabasco", color: "#b0c2f2" }},
+    {id: "24-1", info: { x: 880, y: 130, size: 3, label: "Comalcalco", color: "#b0c2f2" }},
+    {id: "24-2", info: { x: 860, y: 120, size: 3, label: "La Venta", color: "#b0c2f2" }},
+    {id: "24-3", info: { x: 900, y: 120, size: 3, label: "Pomoná", color: "#b0c2f2" }},
+    {id: "24-4", info: { x: 865, y: 85, size: 3, label: "Malpasito", color: "#b0c2f2" }},
+    {id: "24-5", info: { x: 895, y: 85, size: 3, label: "Moral-Reforma", color: "#b0c2f2" }},
+
     {id: "25", info: { x: 1040, y: 140, size: 12, label: "Quintana Roo", color: "#fabfb7"}},
+    {id: "25-1", info: { x: 1040, y: 110, size: 3, label: "Cobá", color: "#fabfb7"}},
+    {id: "25-2", info: { x: 1050, y: 115, size: 3, label: "Chacchoben", color: "#fabfb7"}},
+    {id: "25-3", info: { x: 1060, y: 120, size: 3, label: "Dzibanché", color: "#fabfb7"}},
+    {id: "25-4", info: { x: 1070, y: 125, size: 3, label: "El Meco", color: "#fabfb7"}},
+    {id: "25-5", info: { x: 1030, y: 110, size: 3, label: "El Rey", color: "#fabfb7"}},
+    {id: "25-6", info: { x: 1020, y: 110, size: 3, label: "Kinichná", color: "#fabfb7"}},
+    {id: "25-7", info: { x: 1010, y: 110, size: 3, label: "Kohunlich", color: "#fabfb7"}},
+    {id: "25-8", info: { x: 1040, y: 140, size: 3, label: "Muyil", color: "#fabfb7"}},
+    {id: "25-9", info: { x: 1055, y: 165, size: 3, label: "Oxtankah", color: "#fabfb7"}},
+    {id: "25-10", info: { x: 1065, y: 160, size: 3, label: "San Gervasio", color: "#fabfb7"}},
+    {id: "25-11", info: { x: 1075, y: 165, size: 3, label: "San Miguelito", color: "#fabfb7"}},
+    {id: "25-12", info: { x: 1075, y: 155, size: 3, label: "Tulum-Tancah", color: "#fabfb7"}},
+    {id: "25-13", info: { x: 1020, y: 150, size: 3, label: "Xcaret", color: "#fabfb7"}},
+    {id: "25-14", info: { x: 1010, y: 130, size: 3, label: "Xelhá", color: "#fabfb7"}},
+
     {id: "26", info: { x: 1020, y: 180, size: 12, label: "Yucatan", color: "#fdf9c4" }},
     {id: "27", info: { x: 980, y: 140, size: 12, label: "Campeche", color: "#ffda9e" }},
 
@@ -72,7 +147,16 @@ export const positions = [
     {id: "30", info: { x: 660, y: 135, size: 7, label: "Tlaxcala", color: "#ffe4e1" }},
     {id: "31", info: { x: 620, y: 115, size: 7, label: "Morelos", color: "#d8f8e1" }},
     {id: "32", info: { x: 620, y: 130, size: 5, label: "CDMX", color: "#fcb7af" }},
+
     {id: "33", info: { x: 660, y: 110, size: 12, label: "Puebla", color: "#b0f2c2" }},
+    {id: "33-1", info: { x: 660, y: 95, size: 3, label: "Cantona", color: "#b0f2c2" }},
+    {id: "33-2", info: { x: 660, y: 110, size: 3, label: "Cholula", color: "#b0f2c2" }},
+    {id: "33-3", info: { x: 640, y: 107, size: 3, label: "Los Cerritos", color: "#b0f2c2" }},
+    {id: "33-4", info: { x: 645, y: 120, size: 3, label: "Teteles de Santo Nombre", color: "#b0f2c2" }},
+    {id: "33-5", info: { x: 680, y: 110, size: 3, label: "Tepapayeca", color: "#b0f2c2" }},
+    {id: "33-6", info: { x: 670, y: 120, size: 3, label: "Tehuacán el Viejo", color: "#b0f2c2" }},
+    {id: "33-7", info: { x: 650, y: 100, size: 3, label: "Tepexi el Viejo", color: "#b0f2c2" }},
+    {id: "33-8", info: { x: 655, y: 125, size: 3, label: "Yohualichan", color: "#b0f2c2" }},
 ]
 
     
